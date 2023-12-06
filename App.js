e7cef14e5dd9829ad7ac1963fd9378762f1ae6a2
VicGa <90812185+VicGa@users.noreply.github.com>
2023-12-05 21:23:25 -0300

    front

---
+++ App.js
@@ -0,0 +1,17 @@
+// TINY EDITOR SCRIPT
+tinymce.init({
+  selector: '#referred',
+  branding:false,
+  menubar:false,
+  statusbar: false,
+  toolbar:
+    "forecolor | bold | italic |underline| media | image | emoticons | fullscreen",
+  plugins: "image, emoticons, media , fullscreen",
+  height: 200,
+});
+//  const formulario = document.getElementById('formulario');
+//  formulario.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const contenido = tinymce.activeEditor.getContent({format:'text'});
+//   console.log(contenido);
+//  })
